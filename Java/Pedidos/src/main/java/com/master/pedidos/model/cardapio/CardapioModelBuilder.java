f5d8693c1f430b01b2791d4233ee5d92e183dea1
Vilmar.Ricken <vilmar.ricken@gmail.com>
2015-12-18 14:28:41 -0200

    commit

--- Java/Pedidos/src/main/java/com/master/pedidos/model/cardapio/CardapioModelBuilder.java
+++ Java/Pedidos/src/main/java/com/master/pedidos/model/cardapio/CardapioModelBuilder.java
@@ -1,5 +1,6 @@
 package com.master.pedidos.model.cardapio;
 
+import java.text.NumberFormat;
 import java.util.Set;
 
 import com.master.core.dao.DaoFactory;
@@ -10,64 +11,76 @@ public class CardapioModelBuilder {
 
 	private static final String NL = "\n";
 
-	public static CardapioModel build(String estabelecimento) throws MasterException {
+	public String build(String estabelecimento) throws MasterException {
 		final CardapioDao dao = DaoFactory.getDao(CardapioDao.class);
 		final CardapioModel model = dao.builModel(estabelecimento);
 		final StringBuilder out = new StringBuilder(5 * 1024);
-		CardapioModelBuilder.build(model, out);
-		return model;
+		this.build(model, out);
+		return out.toString();
 	}
 
-	private static void build(CardapioModel model, StringBuilder out) {
+	private void build(CardapioModel model, StringBuilder out) {
 		out.append("<!DOCTYPE html>").append(CardapioModelBuilder.NL);
 		out.append("<html lang=\"pt\">").append(CardapioModelBuilder.NL);
-		CardapioModelBuilder.buildHeader(model, out);
-		CardapioModelBuilder.buildBody(model, out);
+		this.buildHeader(model, out);
+		this.buildBody(model, out);
 		out.append("</html>").append(CardapioModelBuilder.NL);
 	}
 
-	private static void buildBody(CardapioModel model, StringBuilder out) {
+	private void buildBody(CardapioModel model, StringBuilder out) {
 		out.append("<body>").append(CardapioModelBuilder.NL);
-		CardapioModelBuilder.buildBodyHeader(model, out);
-		CardapioModelBuilder.buildBodyNavigation(model, out);
-		CardapioModelBuilder.builBodyContainer(model, out);
+		this.buildBodyHeader(model, out);
+		this.buildBodyNavigation(model, out);
+		this.builBodyContainer(model, out);
 		out.append("</body>").append(CardapioModelBuilder.NL);
 	}
 
-	private static void builBodyContainer(CardapioModel model, StringBuilder out) {
+	private void builBodyContainer(CardapioModel model, StringBuilder out) {
 		out.append("	<div class=\"container\">").append(CardapioModelBuilder.NL);
 		out.append("	<div class=\"row\">").append(CardapioModelBuilder.NL);
-		Set<Bloco> blocos = model.getBlocos();
-		for (Bloco bloco : blocos) {
-			buildBodyBloco(bloco, out);
+		final Set<Bloco> blocos = model.getBlocos();
+		for (final Bloco bloco : blocos) {
+			final Set<Bloco> filhos = bloco.getFillhos();
+			for (final Bloco filho : filhos) {
+				out.append("	<div class=\"col-md-6\">").append(CardapioModelBuilder.NL);
+				this.buildBodyBloco(filho, out);
+				out.append("	</div>").append(CardapioModelBuilder.NL);
+			}
 		}
 		out.append("	</div>").append(CardapioModelBuilder.NL);
 		out.append("	</div>").append(CardapioModelBuilder.NL);
 	}
 
-	private static void buildBodyBloco(Bloco bloco, StringBuilder out) {
-		<div class="panel panel-default">
-		  <div class="panel-body">
-		    Basic panel example
-		  </div>
-		</div>
-		
-		
-		
-		<div class="panel panel-default">
-		  <!-- Default panel contents -->
-		  <div class="panel-heading">Panel heading</div>
-
-		  <!-- Table -->
-		  <table class="table">
-		    ...
-		  </table>
-		</div>
-		
-		
+	private void buildBodyBloco(Bloco bloco, StringBuilder out) {
+		out.append("<table width=\"100%\" border=\"1\">").append(CardapioModelBuilder.NL);
+		out.append("<tr>").append(CardapioModelBuilder.NL);
+		out.append("	<td colspan=\"4\" align=\"center\">").append(bloco.getTitulo()).append("</td>").append(CardapioModelBuilder.NL);
+		out.append("</tr>").append(CardapioModelBuilder.NL);
+		final Set<Produto> produtos = bloco.getProduto();
+		if (produtos != null) {
+			for (final Produto produto : produtos) {
+				this.buildBodyProduto(produto, out);
+			}
+		}
+		out.append("</table>").append(CardapioModelBuilder.NL);
 	}
 
-	private static void buildBodyNavigation(CardapioModel model, StringBuilder out) {
+	private void buildBodyProduto(Produto produto, StringBuilder out) {
+		out.append("<tr>").append(CardapioModelBuilder.NL);
+		out.append("<td width=\"10%\">").append(produto.getCodigo()).append("</td>").append(CardapioModelBuilder.NL);
+		out.append("<td>").append(produto.getNome()).append("</td>").append(CardapioModelBuilder.NL);
+		out.append("<td width=\"20%\" nowrap=\"true\">").append(NumberFormat.getCurrencyInstance().format(10)).append("</td>").append(CardapioModelBuilder.NL);
+		out.append("<td width=\"20%\" nowrap=\"true\"><input type=\"text\" value=\"1\" maxwidth=\"2\" size=\"2\">&nbsp;Add</td>").append(CardapioModelBuilder.NL);
+		out.append("</tr>").append(CardapioModelBuilder.NL);
+		out.append("<tr>").append(CardapioModelBuilder.NL);
+		final String descricao = produto.getDescricao();
+		if (descricao != null) {
+			out.append("	<td colspan=\"4\">").append(descricao).append("</td>").append(CardapioModelBuilder.NL);
+		}
+		out.append("</tr>").append(CardapioModelBuilder.NL);
+	}
+
+	private void buildBodyNavigation(CardapioModel model, StringBuilder out) {
 		out.append("	<nav class=\"navbar navbar-inverse\">").append(CardapioModelBuilder.NL);
 		out.append("		<div class=\"container-fluid\">").append(CardapioModelBuilder.NL);
 		out.append("			<div class=\"navbar-header\">").append(CardapioModelBuilder.NL);
@@ -95,7 +108,7 @@ public class CardapioModelBuilder {
 		out.append("	</nav>").append(CardapioModelBuilder.NL);
 	}
 
-	private static void buildBodyHeader(CardapioModel model, StringBuilder out) {
+	private void buildBodyHeader(CardapioModel model, StringBuilder out) {
 		out.append("	<div class=\"jumbotron\">").append(CardapioModelBuilder.NL);
 		out.append("		<div class=\"container text-center\">").append(CardapioModelBuilder.NL);
 		out.append("			<h1>");
@@ -111,10 +124,10 @@ public class CardapioModelBuilder {
 		out.append("	</div>").append(CardapioModelBuilder.NL);
 	}
 
-	private static void buildHeader(CardapioModel model, StringBuilder out) {
+	private void buildHeader(CardapioModel model, StringBuilder out) {
 		out.append("<head>").append(CardapioModelBuilder.NL);
 		out.append("	<title>Bootstrap Example</title>").append(CardapioModelBuilder.NL);
-		out.append("	<meta charset=\"utf-8\">").append(CardapioModelBuilder.NL);
+		out.append("	<meta charset=\"iso-8859-1\">").append(CardapioModelBuilder.NL);
 		out.append("	<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">").append(CardapioModelBuilder.NL);
 		out.append("	<link rel=\"stylesheet\" href=\"http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css\">").append(CardapioModelBuilder.NL);
 		out.append("	<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js\"></script>").append(CardapioModelBuilder.NL);
--- Java/Pedidos/src/test/java/com/master/cardapio/CardapioTeste.java
+++ Java/Pedidos/src/test/java/com/master/cardapio/CardapioTeste.java
@@ -1,11 +1,16 @@
 package com.master.cardapio;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import org.junit.Test;
 
 import com.master.core.dao.DaoFactory;
 import com.master.core.exception.MasterException;
 import com.master.pedidos.dao.cardapio.CardapioDao;
 import com.master.pedidos.model.cardapio.CardapioModel;
+import com.master.pedidos.model.cardapio.CardapioModelBuilder;
 
 public class CardapioTeste {
 
@@ -14,9 +19,26 @@ public class CardapioTeste {
 		CardapioDao dao;
 		try {
 			dao = DaoFactory.getDao(CardapioDao.class);
-			CardapioModel model = dao.builModel("10596307373059630737305963073784");
+			final CardapioModel model = dao.builModel("10596307373059630737305963073784");
 			System.out.println(model);
-		} catch (MasterException e) {
+		} catch (final MasterException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void buildCardapio() {
+		try {
+			final CardapioModelBuilder b = new CardapioModelBuilder();
+			final String html = b.build("10596307373059630737305963073784");
+			final File file = new File("cardapio.html");
+			System.out.println(file.getAbsolutePath());
+			final FileOutputStream out = new FileOutputStream(file);
+			out.write(html.getBytes());
+			out.close();
+		} catch (final MasterException e) {
+			e.printStackTrace();
+		} catch (final IOException e) {
 			e.printStackTrace();
 		}
 	}
